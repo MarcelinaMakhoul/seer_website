7427f68945d532af2f5091e6bc754aa813ae773d
MarcelinaMakhoul <marcelina.makhoul@theplayball.com>
2025-04-07 19:22:29 +0300

    Fix export pages

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+// app/contact/page.tsx
+
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      {/* Your contact form or content here */}
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ app/vision/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VisionPage = () => {
+  return (
+    <div>VisionPage</div>
+  )
+}
+
+export default VisionPage
